af8673f25de4fdc57260d182bc291245339cbabd
Rickard Nord <rickard@nord.cc>
2019-03-21 15:49:04 +0100

    Bootstrap 4

--- fwlog.au/src/app.ts
+++ fwlog.au/src/app.ts
@@ -1,5 +1,8 @@
 import {Aurelia} from 'aurelia-framework';
 import {PLATFORM} from 'aurelia-pal';
+require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap'); 
+
 export class App {
   public message: string = 'Hello World!';
 }
